f24e52f71a9d022ce91507e1926ba58bae3ca318
Adrian Duran <duradri21@gmail.com>
2023-01-03 16:19:22 -0300

    Integrando product-detail-menu

--- main.js
+++ main.js
@@ -2,15 +2,26 @@ const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const productDetailMenu = document.querySelector('.product-detail');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
 
 navbarEmail.addEventListener('click', showDesktopMenu);
 
 burgerIcon.addEventListener('click', showMobileMenu);
 
+shoppingCartIcon.addEventListener('click', showProductDetailMenu);
+
+function showProductDetailMenu(){
+    productDetailMenu.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+}
+
 function showDesktopMenu (){
     desktopMenu.classList.toggle('inactive');
+    productDetailMenu.classList.add('inactive');
 }
 
 function showMobileMenu (){
     mobileMenu.classList.toggle('inactive');
+    productDetailMenu.classList.add('inactive');
 }
